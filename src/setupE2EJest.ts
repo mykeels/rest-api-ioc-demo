bec2f35c516c6feea8c400a1111ac8512064f476
mykeels <mykehell123@gmail.com>
2023-05-22 21:58:24 -0400

    chore: db seed in jest setup

--- src/setupE2EJest.ts
+++ src/setupE2EJest.ts
@@ -1,19 +1,21 @@
-process.env.DB_TYPE = "mongodb"
+process.env.DB_TYPE = "mongodb";
 
 import supertest from "supertest";
 
 import { createExpressApp } from "./index";
-import { connect } from "./common/db"
+import { connect } from "./common/db";
+import { seed } from "./common/db/seeds";
 
 const app = createExpressApp();
 
 let disconnect: () => Promise<void> = async () => {};
 
 beforeAll(async () => {
-    global.api = supertest(app);
-    disconnect = await connect();
+  global.api = supertest(app);
+  disconnect = await connect();
+  await seed();
 });
 
 afterAll(async () => {
-    await disconnect();
-})
+  await disconnect();
+});
